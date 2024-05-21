3b545b8ec1fe7eba7258e323f827fbb63f5168f8
jonasmichael <jonasmichael2021@outlook.com>
2024-05-21 13:26:47 -0300

    teste

--- news/validators.py
+++ news/validators.py
@@ -2,9 +2,9 @@ from django.core.exceptions import ValidationError
 
 
 def validate_category_length(value):
-    if len(value) == 0 or len(value) < 200:
-        raise ValidationError("Invalid category name.")
+    if len(value) == 0:
+        raise ValidationError("Este campo não pode estar vazio.")
     elif len(value) > 200:
         raise ValidationError(
-            f"tamanho maximo 200 caracteres(tem {len(value)})."
+            f"Certifique-se de que o valor tenha no máximo 200 caracteres (ele possui {len(value)})."  # noqa
         )
