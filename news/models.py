c1d5bd242f4828e6af506b351b1ac734ccdf137e
jonasmichael <jonasmichael2021@outlook.com>
2024-05-21 13:36:13 -0300

    requisito 2

--- news/models.py
+++ news/models.py
@@ -10,16 +10,20 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    class User(models.Model):
-        name = models.CharField(
-            max_length=200, validators=[validate_category_length]
-        )
-        email = models.CharField(
-            max_length=200, validators=[validate_category_length]
-        )
-        password = models.CharField(
-            max_length=200, validators=[validate_category_length]
-        )
-        role = models.CharField(
-            max_length=200, validators=[validate_category_length]
-        )
+
+class User(models.Model):
+    name = models.CharField(
+        max_length=200, validators=[validate_category_length]
+    )
+    email = models.EmailField(
+        max_length=200, validators=[validate_category_length]
+    )
+    password = models.CharField(
+        max_length=200, validators=[validate_category_length]
+    )
+    role = models.CharField(
+        max_length=200, validators=[validate_category_length]
+    )
+
+    def __str__(self):
+        return self.name
