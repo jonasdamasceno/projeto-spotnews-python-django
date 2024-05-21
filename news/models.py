a3a3b38f4a277fb072eb3970eeab66694453c5d1
jonasmichael <jonasmichael2021@outlook.com>
2024-05-21 14:54:00 -0300

    teste

--- news/models.py
+++ news/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-from news.validators import validate_category_length
+from news.validators import (
+    validate_empty_data,
+    validate_max_length,
+    validate_title_length,
+    validate_date_format,
+)
 
 
 class Category(models.Model):
     name = models.CharField(
-        max_length=200, validators=[validate_category_length]
+        max_length=200, validators=[validate_empty_data, validate_max_length]
     )
 
     def __str__(self):
@@ -13,17 +18,40 @@ class Category(models.Model):
 
 class User(models.Model):
     name = models.CharField(
-        max_length=200, validators=[validate_category_length]
+        max_length=200, validators=[validate_empty_data, validate_max_length]
     )
     email = models.EmailField(
-        max_length=200, validators=[validate_category_length]
+        max_length=200, validators=[validate_empty_data, validate_max_length]
     )
     password = models.CharField(
-        max_length=200, validators=[validate_category_length]
+        max_length=200, validators=[validate_empty_data, validate_max_length]
     )
     role = models.CharField(
-        max_length=200, validators=[validate_category_length]
+        max_length=200, validators=[validate_empty_data, validate_max_length]
     )
 
     def __str__(self):
         return self.name
+
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=200,
+        validators=[
+            validate_empty_data,
+            validate_max_length,
+            validate_title_length,
+        ],
+    )
+    content = models.TextField(validators=[validate_empty_data])
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="news"
+    )
+    created_at = models.DateField(validators=[validate_date_format])
+    image = models.ImageField(upload_to="img/", null=True, blank=True)
+    categories = models.ManyToManyField(
+        Category, related_name="news", validators=[validate_empty_data]
+    )
+
+    def __str__(self) -> str:
+        return self.title
--- news/validators.py
+++ news/validators.py
@@ -1,10 +1,25 @@
 from django.core.exceptions import ValidationError
 
 
-def validate_category_length(value):
+def validate_empty_data(value):
     if len(value) == 0:
         raise ValidationError("Este campo não pode estar vazio.")
-    elif len(value) > 200:
+
+
+def validate_max_length(value):
+    if len(value) > 200:
         raise ValidationError(
             f"Certifique-se de que o valor tenha no máximo 200 caracteres (ele possui {len(value)})."  # noqa
+        )
+
+
+def validate_title_length(value):
+    if len(value.split()) == 1:
+        raise ValidationError("O título deve conter pelo menos 2 palavras.")
+
+
+def validate_date_format(value):
+    if not value.strftime("%Y-%m-%d"):
+        raise ValidationError(
+            f"O valor {value} tem um formato de data inválido. Deve ser no formato  YYYY-MM-DD."  # noqa
         )
